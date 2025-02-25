2a143ab94fba42871584593614a6748ada510fad
Christian Fischer <145367937+Lanz33@users.noreply.github.com>
2025-02-25 14:15:37 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,3 +8,5 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"da-notes-88476","appId":"1:253794234582:web:ec21644eb9bb74e1699977","storageBucket":"da-notes-88476.firebasestorage.app","authDomain":"da-notes-88476.firebaseapp.com","messagingSenderId":"253794234582","measurementId":"G-YCCV3MJN3H"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
+
+// Google-API removed from the code above, insert manually
