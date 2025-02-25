3694ac8695696a37c05a34f5172e88459858d594
Christian Fischer <145367937+Lanz33@users.noreply.github.com>
2025-02-25 14:09:00 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"da-notes-88476","appId":"1:253794234582:web:ec21644eb9bb74e1699977","storageBucket":"da-notes-88476.firebasestorage.app","apiKey":"AIzaSyAX7I85ZrDydtyS4_qz9A1WbBJwggy-NZs","authDomain":"da-notes-88476.firebaseapp.com","messagingSenderId":"253794234582","measurementId":"G-YCCV3MJN3H"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"da-notes-88476","appId":"1:253794234582:web:ec21644eb9bb74e1699977","storageBucket":"da-notes-88476.firebasestorage.app","authDomain":"da-notes-88476.firebaseapp.com","messagingSenderId":"253794234582","measurementId":"G-YCCV3MJN3H"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
